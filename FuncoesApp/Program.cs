9e95c2d61092275fed8555b60be4c46c1effc669
AndreNeves30 <neves.andre23@gmail.com>
2025-08-25 09:12:24 -0300

    Add project files.

---
+++ FuncoesApp/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Atalho para printar na tela
+        /// </summary>
+        /// <param name="message">aqui ver oque deve ser exibido</param>
+        static void Print(string message) 
+        {
+            Console.WriteLine(message);
+        }
+        static void Print(double message)
+        {
+            Console.WriteLine(message.ToString());
+        }
+        static void Print(int message)
+        {
+            Console.WriteLine(message.ToString());
+        }
+        static void Print(float message)
+        {
+            Console.WriteLine(message.ToString());
+        }
+        /// <summary>
+        /// Essa função calcula a area do circulo
+        /// </summary>
+        /// <param name="area">aqui fica o raio do circulo</param>
+        /// <returns></returns>
+
+        static Double CalcArea(Double area) 
+        {
+            Double saida = 0;
+            saida = Math.PI* Math.Pow(area,2);
+            return saida;
+        }
+        static void Main(string[] args)
+        {
+            Print("Olá insira seu nome!");
+            String nome = Console.ReadLine();
+            Print(nome + " agora insira a area do circulo que deseja calcular!");
+            Double area = CalcArea(double.Parse(Console.ReadLine()));
+            Print(nome + " a area do circulo será: " + area);
+        }
+    }
+}
